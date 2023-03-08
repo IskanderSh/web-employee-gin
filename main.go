acf887effa051febec850469fe6d6d2226fdad2d
IskanderSh <is2367361@gmail.com>
2023-03-08 12:11:30 +0300

    first commit

--- main.go
+++ main.go
@@ -8,13 +8,10 @@ import (
 func main() {
 	memoryStorage := packs.NewMemoryStorage()
 	handler := packs.NewHandler(memoryStorage)
-
 	router := gin.Default()
-
 	router.POST("/employee", handler.CreateEmployee)
 	router.GET("/employee/:id", handler.GetEmployee)
 	router.PUT("/employee/:id", handler.UpdateEmployee)
 	router.DELETE("/employee/:id", handler.DeleteEmployee)
-
 	router.Run()
 }
